20f2b22de8db8aec945f2b53f415acc01d8cf488
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-01-05 17:15:31 +0600

    create shareTitle component

---
+++ src/component/ShareTitle.jsx
@@ -0,0 +1,12 @@
+
+
+const ShareTitle = (smallTitle, largeTitle) => {
+      return (
+            <div>
+                  <p>---{smallTitle}---</p>
+                  <h1>{largeTitle}</h1>
+            </div>
+      );
+};
+
+export default ShareTitle;
